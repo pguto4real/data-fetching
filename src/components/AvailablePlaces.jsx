0d0d46ddfcd3db3be2d9c59dc0059987e88154ca
oluwamuibiclear <igbokweudoka@gmail.com>
2025-02-01 10:17:28 -0600

    isFetching state

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -4,13 +4,16 @@ import { useEffect } from "react";
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [isFetching, setIsFetching] = useState(false);
 
   useEffect(() => {
     const fetchPlaces = async () => {
+      setIsFetching(true)
       console.log("\n34\n");
       const response = await fetch("http://localhost:3000/places");
       const resData = await response.json();
       setAvailablePlaces(resData.places);
+      setIsFetching(false)
     };
     fetchPlaces();
   }, []);
@@ -19,7 +22,7 @@ export default function AvailablePlaces({ onSelectPlace }) {
     <Places
       title="Available Places"
       places={availablePlaces}
-      isLoading={true}
+      isLoading={isFetching}
       loadingText={"Fetching place data..."}
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
